c010e8e8f22bd1543ae257cb31f5b4cf8d8d3b3e
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 17:35:05 +0100

    Update insert_into.sql

--- resources/insert_into.sql
+++ resources/insert_into.sql
@@ -17,3 +17,12 @@ INSERT INTO Customers
 (CustomerName, City, Country)
 VALUES 
 ('Cardinal', 'Stavanger', 'Norway');
+
+-- To insert multiple rows of data, we use the same INSERT INTO statement, but with multiple values --
+
+INSERT INTO Customers 
+(CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES
+('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway'),
+('Greasy Burger', 'Per Olsen', 'Gateveien 15', 'Sandnes', '4306', 'Norway'),
+('Tasty Tee', 'Finn Egan', 'Streetroad 19B', 'Liverpool', 'L1 0AA', 'UK');
