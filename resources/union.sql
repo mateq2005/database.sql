6947b2cd759f5aa3522cbe80d6d06b4c25c51b3f
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 19:07:52 +0100

    Update union.sql

--- resources/union.sql
+++ resources/union.sql
@@ -24,3 +24,29 @@ UNION ALL
 SELECT City FROM Suppliers
 ORDER BY City;
 
+/* The following SQL statement returns the German cities (only distinct values) from both the "Customers" and the "Suppliers" table */
+
+SELECT City, Country FROM Customers
+WHERE Country='Germany'
+UNION
+SELECT City, Country FROM Suppliers
+WHERE Country='Germany'
+ORDER BY City;
+
+/* The following SQL statement returns the German cities (duplicate values also) from both the "Customers" and the "Suppliers" table */
+
+SELECT City, Country FROM Customers
+WHERE Country='Germany'
+UNION ALL
+SELECT City, Country FROM Suppliers
+WHERE Country='Germany'
+ORDER BY City;
+
+/* The following SQL statement lists all customers and suppliers */
+
+SELECT 'Customer' AS Type, ContactName, City, Country
+FROM Customers
+UNION
+SELECT 'Supplier', ContactName, City, Country
+FROM Suppliers;
+
