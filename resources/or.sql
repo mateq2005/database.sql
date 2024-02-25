0fbf1221c2fbc5d20fad6861a4b20fb05ec1cc1f
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 17:25:26 +0100

    Update or.sql

--- resources/or.sql
+++ resources/or.sql
@@ -2,3 +2,9 @@
 
 SELECT * FROM Customers
 WHERE (Country = 'Germany') OR (Country = 'Spain');
+
+-- Syntax --
+
+SELECT column(n-1), column(n), column(n+1)
+FROM table_name
+WHERE condition(n-1) OR condition(n) OR condition(n-1);
