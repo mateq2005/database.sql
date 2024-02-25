0d910004b224237afab766e267ab9804afb1ab04
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 18:42:42 +0100

    Update resources

---
+++ resources/aliases.sql
@@ -0,0 +1,27 @@
+-- Syntax --
+
+-- When alias is used on column:
+
+SELECT column_name AS alias_name
+FROM table_name;
+
+-- When alias is used on table:
+
+SELECT column_name(s)
+FROM table_name AS alias_name;
+
+/* The following SQL statement creates two aliases,
+one for the CustomerID column and one for the CustomerName column */
+
+SELECT CustomerID AS ID, CustomerName AS Customer
+FROM Customers;
+
+-- Using [square brackets] for aliases with space characters:
+
+SELECT ProductName AS [My Great Products]
+FROM Products;
+
+-- Using "double quotes" for aliases with space characters:
+
+SELECT ProductName AS "My Great Products"
+FROM Products;
