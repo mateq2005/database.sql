79664a0133866c1d4deb4edf993ab950c18fd903
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 19:05:04 +0100

    Update resources

---
+++ resources/union.sql
@@ -0,0 +1,19 @@
+-- Syntax --
+
+SELECT column_name(s) FROM table1
+UNION
+SELECT column_name(s) FROM table2;
+
+/* The UNION operator selects only distinct values by default. To allow duplicate values, use UNION ALL */
+
+SELECT column_name(s) FROM table1
+UNION ALL
+SELECT column_name(s) FROM table2;
+
+/* The following SQL statement returns the cities (only distinct values) from both the "Customers" and the "Suppliers" table */
+
+SELECT City FROM Customers
+UNION
+SELECT City FROM Suppliers
+ORDER BY City;
+
