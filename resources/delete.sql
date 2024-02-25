efee5fcea719b0f66da3e3d2d3d588fc14ece97c
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 17:46:16 +0100

    Update resources

---
+++ resources/delete.sql
@@ -0,0 +1,17 @@
+-- Syntax --
+
+DELETE FROM table_name
+WHERE condition;
+
+-- Deletes the customer "Alfreds Futterkiste" from the "Customers" table --
+
+DELETE FROM Customers 
+WHERE CustomerName='Alfreds Futterkiste';
+
+-- Delete All Records --
+
+DELETE FROM table_name;
+
+-- Remove the Customers table --
+
+DROP TABLE Customers;
