64415b31320da80e075b83245c132ec98d99accc
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 19:00:24 +0100

    Update full_join.sql

--- resources/full_join.sql
+++ resources/full_join.sql
@@ -1,6 +1,14 @@
 -- Syntax --
+
 SELECT column_name(s)
 FROM table1
 FULL JOIN table2
 ON table1.column_name = table2.column_name
 WHERE condition;
+
+-- The following SQL statement selects all customers, and all orders --
+
+SELECT Customers.CustomerName, Orders.OrderID
+FROM Customers
+FULL OUTER JOIN Orders ON Customers.CustomerID=Orders.CustomerID
+ORDER BY Customers.CustomerName;
