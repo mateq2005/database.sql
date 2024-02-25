b619669f37c211e0a3e255d76df168d6c2d16762
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 18:24:25 +0100

    Update like.sql

--- resources/like.sql
+++ resources/like.sql
@@ -8,3 +8,10 @@ WHERE CustomerName LIKE 'a%';
 SELECT column(n-1), column(n), column(n+1)
 FROM table_name
 WHERE column(n) LIKE pattern;
+
+/* Return all customers from a city
+that starts with 'L' followed by one wildcard character,
+then 'nd' and then two wildcard characters */
+
+SELECT * FROM Customers
+WHERE city LIKE 'L_nd__';
