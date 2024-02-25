9e587040af74e31361cd253967c094d61b1912e1
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 17:33:14 +0100

    Update insert_into.sql

--- resources/insert_into.sql
+++ resources/insert_into.sql
@@ -5,3 +5,8 @@ VALUES (value(n-1), value(n), value(n+1));
 
 INSERT INTO table_name
 VALUES (value(n-1), value(n), value(n+1));
+
+-- The following SQL statement inserts a new record in the "Customers" table --
+
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway');
