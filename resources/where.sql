d711800e8e0baf07fb6b4d78deec0094d626bd74
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 17:03:50 +0100

    Update where.sql

--- resources/where.sql
+++ resources/where.sql
@@ -8,3 +8,13 @@ WHERE Country='Mexico';
 SELECT column(n-1), column(n), column(n+1)
 FROM table_name
 WHERE condition;
+
+-- Text Fields vs. Numeric Fields --
+
+SELECT * FROM Customers
+WHERE CustomerID=1;
+
+-- Operators in The WHERE Clause --
+
+SELECT * FROM Customers
+WHERE CustomerID > 80;
