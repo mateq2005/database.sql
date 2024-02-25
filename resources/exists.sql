05fb1e0f5fa901077d4a36f884330af55e03faf1
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 19:22:00 +0100

    Update resources

---
+++ resources/exists.sql
@@ -0,0 +1,18 @@
+-- Syntax --
+
+SELECT column_name(s)
+FROM table_name
+WHERE EXISTS
+(SELECT column_name FROM table_name WHERE condition);
+
+/* The following SQL statement returns TRUE and lists the suppliers with a product price less than 20 */
+
+SELECT SupplierName
+FROM Suppliers
+WHERE EXISTS (SELECT ProductName FROM Products WHERE Products.SupplierID = Suppliers.supplierID AND Price < 20);
+
+/* The following SQL statement returns TRUE and lists the suppliers with a product price equal to 22 */
+
+SELECT SupplierName
+FROM Suppliers
+WHERE EXISTS (SELECT ProductName FROM Products WHERE Products.SupplierID = Suppliers.supplierID AND Price = 22);
