2cfa8d198d3c64a4643917bb1149a1d7187e23c2
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 17:38:36 +0100

    Update null.sql

--- resources/null.sql
+++ resources/null.sql
@@ -16,3 +16,9 @@ WHERE column_name IS NOT NULL;
 SELECT CustomerName, ContactName, Address
 FROM Customers
 WHERE Address IS NULL;
+
+-- The following SQL lists all customers with a value in the "Address" field --
+
+SELECT CustomerName, ContactName, Address
+FROM Customers
+WHERE Address IS NOT NULL;
