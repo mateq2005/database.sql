60b747580cb815488747f31646ebccee90d67e05
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 17:40:59 +0100

    Update resources

---
+++ resources/update.sql
@@ -0,0 +1,11 @@
+-- Syntax --
+
+UPDATE table_name
+SET column(n-1) = value(n-1), column(n) = value(n), column(n+1) = value(n+1)
+WHERE condition;
+
+-- Updates the first customer with a new contact person and a new city. --
+
+UPDATE Customers
+SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'
+WHERE CustomerID = 1;
