203e2c2b5d7aa400d8f31627211042feb87d4bb3
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 17:36:55 +0100

    Update null.sql

--- resources/null.sql
+++ resources/null.sql
@@ -1,12 +1,12 @@
 -- IS NULL Syntax --
 
-SELECT column_names
+SELECT *
 FROM table_name
 WHERE column_name IS NULL;
 
 
 -- IS NOT NULL Syntax --
 
-SELECT column_names
+SELECT *
 FROM table_name
 WHERE column_name IS NOT NULL;
