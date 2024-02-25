57ca09e351c2509d5f09bdf1f8254c704d5b9623
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 19:17:57 +0100

    Update having.sql

--- resources/having.sql
+++ resources/having.sql
@@ -14,3 +14,19 @@ FROM Customers
 GROUP BY Country
 HAVING COUNT(CustomerID) > 5;
 
+/* The following SQL statement lists the number of customers in each country, sorted high to low (Only include countries with more than 5 customers) */
+
+SELECT COUNT(CustomerID), Country
+FROM Customers
+GROUP BY Country
+HAVING COUNT(CustomerID) > 5
+ORDER BY COUNT(CustomerID) DESC;
+
+/* The following SQL statement lists the employees that have registered more than 10 orders */
+
+SELECT Employees.LastName, COUNT(Orders.OrderID) AS NumberOfOrders
+FROM (Orders
+INNER JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID)
+GROUP BY LastName
+HAVING COUNT(Orders.OrderID) > 10;
+
