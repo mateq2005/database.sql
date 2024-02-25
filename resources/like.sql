937d1d500dd020c53b47eae1161f81f13cc284bb
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 18:28:54 +0100

    Update like.sql

--- resources/like.sql
+++ resources/like.sql
@@ -30,3 +30,33 @@ WHERE CustomerName LIKE 'La%';
 
 SELECT * FROM Customers
 WHERE (CustomerName LIKE 'a%') OR (CustomerName LIKE 'b%');
+
+-- Return all customers that ends with 'a' --
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE '%a';
+
+-- Return all customers that starts with "b" and ends with "s" --
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE 'b%s';
+
+-- Return all customers that contains the phrase 'or' --
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE '%or%';
+
+-- Return all customers that starts with "a" and are at least 3 characters in length --
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE 'a__%';
+
+-- Return all customers that have "r" in the second position --
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE '_r%';
+
+-- Return all customers from Spain --
+
+SELECT * FROM Customers
+WHERE Country LIKE 'Spain';
