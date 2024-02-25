78b2b7e7176b31281753af327c23f83faae1d533
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 19:29:00 +0100

    Update insert_into_select.sql

--- resources/insert_into_select.sql
+++ resources/insert_into_select.sql
@@ -10,3 +10,19 @@ INSERT INTO table2 (column1, column2, column3, ...)
 SELECT column1, column2, column3, ...
 FROM table1
 WHERE condition;
+
+-- Copy "Suppliers" into "Customers" (the columns that are not filled with data, will contain NULL) --
+
+INSERT INTO Customers (CustomerName, City, Country)
+SELECT SupplierName, City, Country FROM Suppliers;
+
+-- Copy "Suppliers" into "Customers" (fill all columns) --
+
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+SELECT SupplierName, ContactName, Address, City, PostalCode, Country FROM Suppliers;
+
+-- Copy only the German suppliers into "Customers" --
+
+INSERT INTO Customers (CustomerName, City, Country)
+SELECT SupplierName, City, Country FROM Suppliers
+WHERE Country='Germany';
