24838ef7169f5155bbb70bd5327bdb387f5f51c3
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 17:08:31 +0100

    Update order_by.sql

--- resources/order_by.sql
+++ resources/order_by.sql
@@ -23,3 +23,8 @@ ORDER BY ProductName;
 
 SELECT * FROM Products
 ORDER BY ProductName DESC;
+
+-- This means that it orders by Country, but if some rows have the same Country, it orders them by CustomerName --
+
+SELECT * FROM Customers
+ORDER BY Country, CustomerName;
