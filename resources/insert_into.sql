77575fccce8e76088008fe9aca783e0ab44daf04
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 17:32:23 +0100

    Update resources

---
+++ resources/insert_into.sql
@@ -0,0 +1,7 @@
+-- Syntax --
+
+INSERT INTO table_name (column(n-1), column(n), column(n+1))
+VALUES (value(n-1), value(n), value(n+1));
+
+INSERT INTO table_name
+VALUES (value(n-1), value(n), value(n+1));
