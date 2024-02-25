fb39e2f856cc6441d6c2e9a02974dde72d682acb
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 16:56:57 +0100

    Update distinct.sql

--- resources/distinct.sql
+++ resources/distinct.sql
@@ -1,3 +1,9 @@
 -- Statement is used to return only distinct (different) values --
 
 SELECT DISTINCT Country FROM Customers;
+
+-- Syntax --
+
+SELECT DISTINCT column1, column2, ...
+FROM table_name;
+
