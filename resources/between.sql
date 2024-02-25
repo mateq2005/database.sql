b8e95930b1fce69e6c0a04b5ea4b713e641a28c5
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 18:37:58 +0100

    Update between.sql

--- resources/between.sql
+++ resources/between.sql
@@ -8,3 +8,23 @@ WHERE Price BETWEEN 10 AND 20;
 SELECT column_name(s)
 FROM table_name
 WHERE column_name BETWEEN value(n) AND value(m);
+
+-- To display the products outside the range of the previous example, use NOT BETWEEN --
+
+SELECT * FROM Products
+WHERE Price NOT BETWEEN 10 AND 20;
+
+/* The following SQL statement selects all products with a price between 10 and 20. 
+In addition, the CategoryID must be either 1,2, or 3 */
+
+SELECT * FROM Products
+WHERE (Price BETWEEN 10 AND 20)
+AND CategoryID IN (1,2,3);
+
+/* The following SQL statement selects all products 
+with a ProductName alphabetically between Carnarvon Tigers and Mozzarella di Giovanni */
+
+SELECT * FROM Products
+WHERE ProductName BETWEEN 'Carnarvon Tigers' AND 'Mozzarella di Giovanni'
+ORDER BY ProductName;
+
