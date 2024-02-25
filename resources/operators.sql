305989b540d5140311a9a1b3f9fc00000f03a2dc
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 19:40:17 +0100

    Update resources

---
+++ resources/operators.sql
@@ -0,0 +1,22 @@
+/*
++	Add	
+-	Subtract	
+*	Multiply	
+/	Divide	
+%	Modulo
+*/
+
+/*
+&	Bitwise AND
+|	Bitwise OR
+^	Bitwise exclusive OR 
+*/
+
+/*
+=	Equal to	
+>	Greater than	
+<	Less than	
+>=	Greater than or equal to	
+<=	Less than or equal to	
+<>	Not equal to
+*/
