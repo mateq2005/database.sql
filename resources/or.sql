51273c88b06f20c293642efe81c4f761370e85c9
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-25 17:26:24 +0100

    Update or.sql

--- resources/or.sql
+++ resources/or.sql
@@ -8,3 +8,8 @@ WHERE (Country = 'Germany') OR (Country = 'Spain');
 SELECT column(n-1), column(n), column(n+1)
 FROM table_name
 WHERE condition(n-1) OR condition(n) OR condition(n-1);
+
+-- At Least One Condition Must Be True --
+
+SELECT * FROM Customers
+WHERE (City = 'Berlin') OR (CustomerName LIKE 'G%') OR (Country = 'Norway');
